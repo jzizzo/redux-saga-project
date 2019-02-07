da117d37620c1cfd3c6ec0aeddc1702aaedef793
Joseph Zizzo <josephzizzo@Josephs-MBP.tafmo.net>
2019-02-06 18:42:35 -0800

    Add createUser functionality

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, fork, put } from 'redux-saga/effects';
+import { takeEvery, takeLatest, call, fork, put } from 'redux-saga/effects';
 import * as actions from '../actions/users';
 import * as api from '../api/users';
 
@@ -17,8 +17,25 @@ function* watchGetUsersRequest() {
   yield takeEvery(actions.Types.GET_USERS_REQUEST, getUsers);
 }
 
+function* createUser(action) {
+  try {
+    yield call(api.createUser, {
+      firstName: action.payload.firstName, 
+      lastName: action.payload.lastName
+    });
+    yield call(getUsers);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function* watchCreateUserRequest() {
+  yield takeLatest(actions.Types.CREATE_USER_REQUEST, createUser);
+}
+
 const usersSages = [
-  fork(watchGetUsersRequest)
+  fork(watchGetUsersRequest),
+  fork(watchCreateUserRequest)
 ];
 
 export default usersSages;
